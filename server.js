9efab5b192208d45f844c01ad884b151513cbb35
Mitch Kroska <kromitj@gmail.com>
2019-02-21 06:47:02 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 	mongoose.Promise = global.Promise
 	mongoose.connect('mongodb://localhost/Reportdb', { useNewUrlParser: true }) 
-const REPORT = require('./api/models/report') //created model loading here --- Why does the app break if I remove????? it breaks if put after the routes(app)	
+		const REPORT = require('./api/models/report') //created model loading here --- Why does the app break if I remove????? it breaks if put after the routes(app)	
 
 
 const express = require('express')
