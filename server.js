8d1f5a6669654fbfffa05452592b3b889776d206
Mitch Kroska <kromitj@gmail.com>
2019-02-21 05:39:27 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -5,21 +5,28 @@ const express = require('express')
 
 const app = express()
 	const bodyParser = require('body-parser')
+
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: true }))
+
 	app.listen(PORT)			
+
 	app.use(function(req, res) {
 	  res.status(404).send({url: req.originalUrl + ' not found'})
 	});
+
 const routes = require('./api/routes/reportRoutes'); //importing route
 	routes(app); //register the route
+
 const mongoose = require('mongoose')
 	mongoose.Promise = global.Promise
 	mongoose.connect('mongodb://localhost/Reportdb', { useNewUrlParser: true }) 
-console.log('Report RESTful API server started on: ' + PORT);
+
 const cron = require("node-cron")
 	cron.schedule("* * * * *", function() {
 		const Reports = require('./api/controllers/reportController')
 		Reports.crawl2()
 	});
+	
+console.log('Report RESTful API server started on: ' + PORT);
 
