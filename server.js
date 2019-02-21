99d19a3f01ff81dd3b32783c04b48af675738adc
Mitch Kroska <kromitj@gmail.com>
2019-02-21 07:01:56 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const mongoose = require('mongoose')
 const express = require('express')
 	const app = express()
 		const PORT = process.env.PORT || 3000
-		app.listen(PORT)
-			const bodyParser = require('body-parser')
-			app.use(bodyParser.json())
-			app.use(bodyParser.urlencoded({ extended: true }))
+			app.listen(PORT)
+				const bodyParser = require('body-parser')
+				app.use(bodyParser.json())
+				app.use(bodyParser.urlencoded({ extended: true }))
 		app.use(function(req, res) {
 		  res.status(404).send({url: req.originalUrl + ' not found'})
 		});
