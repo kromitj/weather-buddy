2022497b9b41796fd7f834fcd7cab0d79b6eced7
Mitch Kroska <kromitj@gmail.com>
2019-02-21 05:23:48 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const REPORT = require('./api/models/report') //created model loading here --- W
 	const express = require('express')
 		const app = express()
 			app.listen(PORT)			
-			const bodyParser = require('body-parser')
-				app.use(bodyParser.json())
-				app.use(bodyParser.urlencoded({ extended: true }))
 			app.use(function(req, res) {
-				  res.status(404).send({url: req.originalUrl + ' not found'})
+			  res.status(404).send({url: req.originalUrl + ' not found'})
 			});
+			const bodyParser = require('body-parser')
+			app.use(bodyParser.json())
+			app.use(bodyParser.urlencoded({ extended: true }))
 		const routes = require('./api/routes/reportRoutes'); //importing route
 			routes(app); //register the route
-	const mongoose = require('mongoose')
-		mongoose.Promise = global.Promise
-		mongoose.connect('mongodb://localhost/Reportdb', { useNewUrlParser: true }) 
+		const mongoose = require('mongoose')
+			mongoose.Promise = global.Promise
+			mongoose.connect('mongodb://localhost/Reportdb', { useNewUrlParser: true }) 
 		console.log('Report RESTful API server started on: ' + PORT);
-			const cron = require("node-cron")
-				cron.schedule("* * * * *", function() {
-					const Reports = require('./api/controllers/reportController')
-					Reports.crawl2()
-				});
+		const cron = require("node-cron")
+			cron.schedule("* * * * *", function() {
+				const Reports = require('./api/controllers/reportController')
+				Reports.crawl2()
+			});
 
