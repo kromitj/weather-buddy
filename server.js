5484c0585b867d6f4487b3f37751a83271dbb687
Mitch Kroska <kromitj@gmail.com>
2019-02-21 04:17:38 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -1,36 +1,23 @@
 const PORT = process.env.PORT || 3000
-
-
 const REPORT = require('./api/models/report') //created model loading here --- Why does the app break if I remove????? it breaks if put after the routes(app)
-
 	const express = require('express')
 		const app = express()
-		const bodyParser = require('body-parser')
-
-			app.use(bodyParser.json())
-			app.use(bodyParser.urlencoded({ extended: true }))
-
-			app.listen(PORT)
-			
-			app.use(function(req, res) {
-			  res.status(404).send({url: req.originalUrl + ' not found'})
-			});
-
+			const bodyParser = require('body-parser')
+				app.use(bodyParser.json())
+				app.use(bodyParser.urlencoded({ extended: true }))
+				app.listen(PORT)			
+				app.use(function(req, res) {
+				  res.status(404).send({url: req.originalUrl + ' not found'})
+				});
 	const routes = require('./api/routes/reportRoutes'); //importing route
 		routes(app); //register the route
-
-
 	const mongoose = require('mongoose')
 		mongoose.Promise = global.Promise
 		mongoose.connect('mongodb://localhost/Reportdb', { useNewUrlParser: true }) 
-
-
 	const cron = require("node-cron")
 		cron.schedule("* * * * *", function() {
 			const Reports = require('./api/controllers/reportController')
 				Reports.crawl2()
 		});
-
-
 	console.log('Report RESTful API server started on: ' + PORT);
 
