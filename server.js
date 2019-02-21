892abecd09ca576d3b7f597b347f70a6f97e4470
Mitch Kroska <kromitj@gmail.com>
2019-02-21 04:04:51 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const REPORT = require('./api/models/report') //created model loading here --- W
 const routes = require('./api/routes/reportRoutes'); //importing route
 	const express = require('express')
 		const app = express()
-			app.listen(PORT)
-
 			const bodyParser = require('body-parser')
+
 			app.use(bodyParser.json())
 			app.use(bodyParser.urlencoded({ extended: true }))
+
+			app.listen(PORT)
 			
 			app.use(function(req, res) {
 			  res.status(404).send({url: req.originalUrl + ' not found'})
@@ -30,7 +31,7 @@ const Reports = require('./api/controllers/reportController')
 	cron.schedule("* * * * *", function() {
 		Reports.crawl2()
 	});
-	
+
 
 console.log('Report RESTful API server started on: ' + PORT);
 
