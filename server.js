2262c7f0da260a3d777a2265faddddf73d058f2e
Mitch Kroska <kromitj@gmail.com>
2019-02-21 06:37:19 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const PORT = process.env.PORT || 3000
 const REPORT = require('./api/models/report') //created model loading here --- Why does the app break if I remove????? it breaks if put after the routes(app)	
 const mongoose = require('mongoose')
 	mongoose.Promise = global.Promise
 	mongoose.connect('mongodb://localhost/Reportdb', { useNewUrlParser: true }) 
+const PORT = process.env.PORT || 3000
 const express = require('express')
 	const app = express()
 		app.listen(PORT)
