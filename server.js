bcd2cf16bf93129dbdbc8ed5e83308caf7482395
Mitch Kroska <kromitj@gmail.com>
2019-02-21 05:48:40 -0700

    edit, recator server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const express = require('express')
 
 const app = express()
 	app.listen(PORT)
-
 	const bodyParser = require('body-parser')
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: true }))
